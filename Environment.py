787e52e84623aa50f3338d73e8b381ef4752f7fc
Krishna <krishnaprasaad.r12@gmail.com>
2025-05-28 23:57:24 +0530

    Added an Environment

---
+++ Environment.py
@@ -0,0 +1,34 @@
+import random
+
+class NumberStabilizer:
+    def __init__(self, start_number=None):
+        self.done = False
+        self.state = start_number
+        
+    def set_state(self):
+        return random.randint(-5, 5)
+             
+    def reset(self, value=None):
+        self.state = value if value is not None else self.set_state()
+        self.done = False
+        return self.state
+    
+    def step(self, action, reward=0):
+        """
+        Actions: +1 or -1
+        Return: (New_State, reward, done=True)
+        """
+
+        self.state += action
+
+        if -1<=self.state<=1:
+            reward = 1
+            self.done = True
+        else:
+            reward = -1
+
+        return self.state, reward, self.done
+
+    def render(self):
+        print(f"Number: {self.state}")
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Number-Stabilizer
+    Given any number i want to bring it into the range [-1, 1], using +1 or -1.
